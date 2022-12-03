a9209f73e75e2cfa823cae260f729c0739bb6db9
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-03 22:10:52 +0100

    Enable simple controls

---
+++ src/components/canvas/Player.tsx
@@ -0,0 +1,45 @@
+import { Vector3 } from 'three'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useKeyboardControls } from '@react-three/drei'
+import { useRef } from 'react'
+import { RigidBody, RigidBodyApi, useRapier } from '@react-three/rapier'
+
+const SPEED = 5
+const direction = new Vector3()
+const frontVector = new Vector3()
+const sideVector = new Vector3()
+
+export function Player() {
+  const [, get] = useKeyboardControls()
+  const ref = useRef<RigidBodyApi>()
+  const { camera } = useThree()
+
+  useFrame((state) => {
+    if (!ref.current) return
+
+    const { forward, backward, left, right, jump, descend } = get()
+
+    const velocity = ref.current.linvel()
+    camera.position.set(...ref.current.translation().toArray())
+
+    // @ts-ignore:next-line
+    frontVector.set(0, 0, backward - forward)
+    // @ts-ignore:next-line
+    sideVector.set(left - right, 0, 0)
+
+    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(camera.rotation)
+
+    // @ts-ignore:next-line
+    ref.current.setLinvel({ x: direction.x, y: (jump - descend) * SPEED, z: direction.z })
+  })
+  return (
+    <RigidBody
+      ref={ref}
+      colliders={false}
+      mass={1}
+      type='dynamic'
+      position={[0, 10, 0]}
+      enabledRotations={[false, false, false]}
+    />
+  )
+}
--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,15 +1,30 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, Sky } from '@react-three/drei'
+import { KeyboardControls, PointerLockControls, Preload, Sky } from '@react-three/drei'
+import { Player } from './Player'
+import { Physics } from '@react-three/rapier'
 
 export default function Scene({ children, ...props }) {
   return (
-    <Canvas {...props}>
-      <Sky azimuth={1} inclination={0.6} distance={1000} />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      {children}
-      <Preload all />
-      <OrbitControls />
-    </Canvas>
+    <KeyboardControls
+      map={[
+        { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
+        { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
+        { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
+        { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
+        { name: 'jump', keys: ['Space'] },
+        { name: 'descend', keys: ['c', 'C', 'Shift'] },
+      ]}>
+      <Canvas {...props}>
+        <Sky azimuth={1} inclination={0.6} distance={1000} />
+        <ambientLight />
+        <Physics>
+          <Player />
+        </Physics>
+        <pointLight position={[10, 10, 10]} />
+        {children}
+        <Preload all />
+        <PointerLockControls />
+      </Canvas>
+    </KeyboardControls>
   )
 }
