4d9cd7c167c3cd09c5f70efc8c3b4ec62847ef32
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-04 22:59:47 +0100

    Configure birchtree and add Trex!

---
+++ src/components/canvas/BirchTree_5.tsx
@@ -0,0 +1,54 @@
+import React, { useLayoutEffect } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { DoubleSide, Mesh } from 'three'
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+
+export function BirchTree() {
+  const { nodes, materials } = useGLTF('/BirchTree_5.gltf')
+  const [colorMap, normalMap] = useLoader(TextureLoader, [
+    '/textures/BirchTree_Bark.png',
+    '/textures/BirchTree_Bark_Normal.png',
+  ])
+
+  const [leavesColorMap] = useLoader(TextureLoader, ['/textures/BirchTree_Leaves.png'])
+
+  useLayoutEffect(() => {
+    Object.assign(materials.BirchTree_Bark, {
+      normalMap: normalMap,
+      map: colorMap,
+    })
+  }, [materials.BirchTree_Bark, colorMap, normalMap])
+
+  useLayoutEffect(() => {
+    Object.assign(materials.BirchTree_Leaves, {
+      map: leavesColorMap,
+      // alphaTest: 0.5,
+      transparent: true,
+      side: DoubleSide,
+    })
+  }, [materials.BirchTree_Leaves, leavesColorMap])
+
+  return (
+    <group dispose={null}>
+      <mesh geometry={(nodes.Cube009 as Mesh).geometry} material={materials.BirchTree_Bark} />
+      <mesh geometry={(nodes.Cube009_1 as Mesh).geometry} material={materials.BirchTree_Leaves} />
+    </group>
+  )
+}
+
+export function BirchTreeWithColors() {
+  const { nodes } = useGLTF('/BirchTree_5.gltf')
+  return (
+    <group>
+      <mesh geometry={(nodes.Cube009 as Mesh).geometry}>
+        <meshStandardMaterial color='#ff0000' />
+      </mesh>
+      <mesh geometry={(nodes.Cube009_1 as Mesh).geometry}>
+        <meshStandardMaterial color='#00BB00' />
+      </mesh>
+    </group>
+  )
+}
+
+useGLTF.preload('/BirchTree_5.gltf')
--- src/components/canvas/Logo.tsx
+++ src/components/canvas/Logo.tsx
@@ -1,39 +1,13 @@
-import * as THREE from 'three'
-import { useLayoutEffect } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { Material, Mesh } from 'three'
+import { Trex } from '../Trex'
+import { BirchTree } from './BirchTree_5'
 import { Ocean } from './Ocean'
 
-function BirchTree() {
-  const result = useGLTF('/BirchTree_5.gltf')
-  const { nodes, scene, materials } = result
-
-  console.log(result, nodes, scene, materials)
-
-  console.log(nodes.BirchTree_5.children[0])
-
+export default function Logo() {
   return (
     <>
-      <mesh geometry={(nodes.BirchTree_5.children[0] as Mesh).geometry}>
-        <meshStandardMaterial color='#ff0000' />
-      </mesh>
+      {/* <BirchTree /> */}
+      <Trex />
       <Ocean />
     </>
-
-    /* <mesh material={materials.BirchTree_Leaves} geometry={(nodes.BirchTree_5.children[1] as Mesh).geometry} /> */
   )
-  // return <primitive object={nodes.BirchTree_5.children[0]} />
-}
-
-// function TreeTrunk() {
-//   const { nodes, materials } = useGLTF('/BirchTree_5.gltf', true)
-//   console.log(nodes, materials)
-
-//   return <mesh material={materials.BirchTree_Bark} geometry={(nodes. as Mesh).geometry} />
-// }
-
-export default function Logo() {
-  // TreeTrunk()
-
-  return <BirchTree />
 }
