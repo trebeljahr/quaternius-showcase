9d30d991a391add64d97f97352615a096b4e961c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-05 22:19:51 +0100

    Fix Trex movements

--- src/components/canvas/PlayerController.tsx
+++ src/components/canvas/PlayerController.tsx
@@ -2,83 +2,60 @@ import { Group, Quaternion, Vector3 } from 'three'
 import { useFrame } from '@react-three/fiber'
 import { useKeyboardControls } from '@react-three/drei'
 import { PropsWithChildren, useRef } from 'react'
-import { RigidBodyApi } from '@react-three/rapier'
-import { useThirdPersonCamera } from '@/hooks/useThirdPersonCamera'
+// import { useThirdPersonCamera } from '@/hooks/useThirdPersonCamera'
 
-const decceleration = new Vector3(-0.0005, -0.0001, -5.0)
-const acceleration = new Vector3(10, 2.5, 500.0)
-const velocity = new Vector3(0, 0, 0)
+const rotationSpeed = 20
 
 export function PlayerController({ children }: PropsWithChildren) {
   const [, get] = useKeyboardControls()
 
   const group = useRef<Group>()
 
-  useThirdPersonCamera({ target: group.current })
-
   useFrame((state) => {
     if (!group.current) return
-    const elapsedTime = state.clock.getDelta()
 
     const { forward, backward, left, right } = get()
-    const controlObject = group.current
+
     const Q = new Quaternion()
     const A = new Vector3()
-    const R = controlObject.quaternion.clone()
+    const elapsedTime = state.clock.getDelta()
 
-    const acc = acceleration.clone()
-
-    const frameDecceleration = new Vector3(
-      velocity.x * decceleration.x,
-      velocity.y * decceleration.y,
-      velocity.z * decceleration.z,
-    )
-    frameDecceleration.multiplyScalar(elapsedTime)
-    frameDecceleration.z =
-      Math.sign(frameDecceleration.z) * Math.min(Math.abs(frameDecceleration.z), Math.abs(velocity.z))
-
-    velocity.add(frameDecceleration)
-
-    if (forward) {
-      velocity.z += acc.z * elapsedTime
-    }
-    if (backward) {
-      velocity.z -= acc.z * elapsedTime
-    }
     if (left) {
       A.set(0, 1, 0)
-      Q.setFromAxisAngle(A, 4.0 * Math.PI * elapsedTime * acceleration.y)
-      R.multiply(Q)
+      Q.setFromAxisAngle(A, rotationSpeed * Math.PI * elapsedTime)
+      group.current.quaternion.multiply(Q)
     }
     if (right) {
       A.set(0, 1, 0)
-      Q.setFromAxisAngle(A, 4.0 * -Math.PI * elapsedTime * acceleration.y)
-      R.multiply(Q)
+      Q.setFromAxisAngle(A, rotationSpeed * -Math.PI * elapsedTime)
+      group.current.quaternion.multiply(Q)
     }
 
-    controlObject.quaternion.copy(R)
+    const vel = new Vector3(0, 0, 0)
+    if (forward) {
+      vel.set(0, 0, 1)
+    }
 
-    const oldPosition = new Vector3()
-    oldPosition.copy(controlObject.position)
+    if (backward) {
+      vel.set(0, 0, -1)
+    }
 
     const forwardVec = new Vector3(0, 0, 1)
-    forwardVec.applyQuaternion(controlObject.quaternion)
+    forwardVec.applyQuaternion(group.current.quaternion)
     forwardVec.normalize()
 
-    const sideways = new Vector3(1, 0, 0)
-    sideways.applyQuaternion(controlObject.quaternion)
-    sideways.normalize()
+    const sidewaysVec = new Vector3(1, 0, 0)
+    sidewaysVec.applyQuaternion(group.current.quaternion)
+    sidewaysVec.normalize()
 
-    sideways.multiplyScalar(velocity.x * elapsedTime)
-    forwardVec.multiplyScalar(velocity.z * elapsedTime)
+    sidewaysVec.multiplyScalar(vel.x)
+    forwardVec.multiplyScalar(vel.z)
 
-    controlObject.position.add(forwardVec)
-    controlObject.position.add(sideways)
-
-    group.current.position.copy(controlObject.position)
-
-    console.log(group.current.position)
+    group.current.position.add(forwardVec)
+    group.current.position.add(sidewaysVec)
   })
+
+  // useThirdPersonCamera({ target: group.current })
 
   return <group ref={group}>{children}</group>
 }
