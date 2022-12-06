88afcfc5663e5750efb1f3f3f68ee31ed53c6403
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-06 21:25:00 +0100

    Fix doubled up imports

--- src/components/quaternius/nature_pack/Grass.tsx
+++ src/components/quaternius/nature_pack/Grass.tsx
@@ -17,7 +17,7 @@ type GLTFResult = GLTF & {
 }
 
 export function Model(props: JSX.IntrinsicElements['group']) {
-  const { nodes, materials } = useGLTF('/glb/nature_pack/glb/nature_pack/Grass.glb') as unknown as GLTFResult
+  const { nodes, materials } = useGLTF('/glb/nature_pack/Grass.glb') as unknown as GLTFResult
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Grass.geometry} material={materials.Green} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
--- src/components/quaternius/nature_pack/Plant_1.tsx
+++ src/components/quaternius/nature_pack/Plant_1.tsx
@@ -17,7 +17,7 @@ type GLTFResult = GLTF & {
 }
 
 export function Model(props: JSX.IntrinsicElements['group']) {
-  const { nodes, materials } = useGLTF('/glb/nature_pack/glb/nature_pack/Plant_1.glb') as unknown as GLTFResult
+  const { nodes, materials } = useGLTF('/glb/nature_pack/Plant_1.glb') as unknown as GLTFResult
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Plant_1.geometry} material={materials.Green} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
