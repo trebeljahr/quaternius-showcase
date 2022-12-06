8a920fcb6d8853eaedbea1763995f09c80d98cf7
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-06 15:05:03 +0100

    Align items nicely

--- src/components/canvas/QuaterniusShowcase.tsx
+++ src/components/canvas/QuaterniusShowcase.tsx
@@ -1,8 +1,8 @@
 import { Physics } from '@react-three/rapier'
 import { Vector3 } from 'three'
 import * as NatureModels from '../quaternius/nature_pack'
-// import * as CropModels from '../quaternius/crops_pack'
-// import * as RealTimeStrategyModels from '../quaternius/fantasy_real_time_strategy'
+import * as CropModels from '../quaternius/crops_pack'
+import * as RealTimeStrategyModels from '../quaternius/fantasy_real_time_strategy'
 
 import { FlyingPlayer } from './FlyingPlayer'
 import { Plane } from './ThirdPersonDemo'
@@ -16,16 +16,23 @@ export default function QuaterniusShowcase() {
       <Plane />
       {Object.values(NatureModels).map((Component, i) => {
         const rowSize = 10
-        return <Component key={i} position={new Vector3((i % rowSize) * 3, 0, Math.floor(i / rowSize) * 3)} />
+
+        return (
+          <Component key={i} position={new Vector3(((i % rowSize) + 1) * 3, 0, (Math.floor(i / rowSize) + 1) * 3)} />
+        )
       })}
-      {/* {Object.values(CropModels).map((Component, i) => {
+      {Object.values(CropModels).map((Component, i) => {
         const rowSize = 10
-        return <Component key={i} position={new Vector3((i % rowSize) * -3, 0, Math.floor(i / rowSize) * 3)} />
+        return (
+          <Component key={i} position={new Vector3(((i % rowSize) + 1) * -3, 0, (Math.floor(i / rowSize) + 1) * 3)} />
+        )
       })}
       {Object.values(RealTimeStrategyModels).map((Component, i) => {
         const rowSize = 10
-        return <Component key={i} position={new Vector3((i % rowSize) * -3, 0, Math.floor(i / rowSize) * -3)} />
-      })} */}
+        return (
+          <Component key={i} position={new Vector3(((i % rowSize) + 1) * -3, 0, (Math.floor(i / rowSize) + 1) * -3)} />
+        )
+      })}
     </>
   )
 }
