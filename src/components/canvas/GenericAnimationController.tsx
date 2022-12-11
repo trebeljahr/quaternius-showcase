3b00a02926c02c1f816fe4cae0d92ac59dc0231f
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-11 10:35:11 +0100

    Fix leva bug

--- src/components/canvas/GenericAnimationController.tsx
+++ src/components/canvas/GenericAnimationController.tsx
@@ -1,7 +1,7 @@
 import { usePrevious } from '@/hooks/usePrevious'
 import { In } from '@/pages/quaternius/[id]'
 import { folder, Leva, useControls } from 'leva'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { AnimationAction } from 'three'
 
 export function LevaStyled() {
@@ -12,14 +12,35 @@ export function LevaStyled() {
 }
 
 export function GenericAnimationController({ actions }: { actions: Record<string, AnimationAction> }) {
-  const { animation } = useControls({ animations: folder({ animation: { options: actions } }) })
-  const previousAnimation = usePrevious(animation)
+  const actionNames = Object.keys(actions)
+  const defaultAction = actionNames.find((name) => name.toLowerCase().includes('idle')) || actionNames[0]
+
+  const [{ animation }, set] = useControls(
+    () => ({
+      animations: folder({
+        animation: {
+          options: actionNames,
+          value: defaultAction,
+        },
+      }),
+    }),
+    [defaultAction, actionNames],
+  )
 
   useEffect(() => {
+    console.log(defaultAction)
+    set({ animation: defaultAction })
+  }, [defaultAction, set])
+  //   const previousAnimation = usePrevious(animation)
+
+  useEffect(() => {
+    console.log(animation)
     const fadeDuration = 0.5
-    previousAnimation?.fadeOut(fadeDuration)
-    animation?.reset().fadeIn(fadeDuration).play()
-  }, [previousAnimation, animation])
+    actions[animation]?.reset().fadeIn(fadeDuration).play()
+    return () => {
+      actions[animation]?.fadeOut(fadeDuration).stop()
+    }
+  }, [animation, actions])
 
   return (
     <In>
