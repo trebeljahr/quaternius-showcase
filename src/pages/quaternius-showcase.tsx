7665b88b7357bfd334465b453885ce3afa38757a
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-11 16:06:48 +0100

    Further cleanup

--- src/pages/quaternius-showcase.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const QuaterniusShowcase = dynamic(() => import('@/components/canvas/QuaterniusShowcase'), { ssr: false })
-
-export default function Page() {
-  return <></>
-}
-
-Page.canvas = () => <QuaterniusShowcase />
