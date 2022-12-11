07e7f585b4d0649de30c2f4f559e2d09d3ad6cb5
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-11 16:05:43 +0100

    Remove unnecessary stuff

--- src/pages/car-demo.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const CarDemo = dynamic(() => import('@/components/canvas/CarDemo'), { ssr: false })
-
-export default function Page() {
-  return <></>
-}
-
-Page.canvas = () => <CarDemo></CarDemo>
