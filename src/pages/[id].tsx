ba20517e034451d81ab8adfa705f50adb85c760f
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-19 12:39:18 +0100

    Add better styling

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -12,14 +12,13 @@ import { useWindowSize } from '@/hooks/useWindowSize'
 import { Group } from 'three'
 import { DownloadIcon } from '@/components/dom/DownloadIcon'
 
-const activeSide =
-  'z-[1500] font-leva text-sm	w-60 bg-leva-dark h-screen  transform transition-all fixed duration-700 text-leva-white p-2'
-const hiddenSide =
-  'z-[1500] font-leva text-sm w-60 bg-leva-dark h-screen  transform transition-all fixed duration-700 text-leva-white p-2 -translate-x-60'
-const activeButton =
+const sideStyle =
+  'z-[1500] overflow-x-hidden overflow-y-auto font-leva text-sm	w-60 bg-leva-dark h-screen  transform transition-all fixed duration-700 text-leva-white p-2'
+
+const downloadStyles = 'absolute left-0 z-20 flex justify-center transition-all transform duration-700 bottom-1'
+
+const buttonStyle =
   'z-[1500] font-leva text-sm	absolute w-10 h-10 bg-yellow-400 hover:w-11 hover:h-11 top-0 cursor-pointer transition-all transform duration-700 flex items-center justify-center'
-const normalButton =
-  'z-[1500] font-leva text-sm	absolute w-10 h-10 bg-yellow-400 hover:w-11 hover:h-11 top-0 cursor-pointer transition-all transform duration-700 flex items-center justify-center translate-x-60'
 
 const navButton =
   'z-[1500] font-leva text-sm w-10 h-10 bg-leva-dark text-leva-white cursor-pointer hover:bg-leva-medium'
@@ -27,9 +26,22 @@ const navButton =
 const t = tunnel()
 export const { Out, In } = t
 
-export default function Page({ id }: { id: string }) {
+export default function Page() {
+  return <Out />
+}
+
+type Ids = keyof typeof AllModels
+
+function CanvasComponent({ id }: { id: Ids }) {
+  const [index, setIndex] = useState(0)
   const [open, setOpen] = useState(false)
+
+  const [components, setComponents] = useState<{ Component: ComponentType<GroupProps>; key: string }[]>([])
   const { width } = useWindowSize()
+
+  const modelRef = useRef<Group>()
+  const stopped = useRef(false)
+
   const toggleOpen = () => {
     setOpen(!open)
   }
@@ -46,68 +58,17 @@ export default function Page({ id }: { id: string }) {
     }
   }, [width])
 
-  return (
-    <>
-      <Out />
-      <button id='close-btn' className={open ? normalButton : activeButton} onClick={toggleOpen}>
-        {open ? '<' : '>'}
-      </button>
-      <div id='' style={{ overflow: 'hidden', overflowY: 'auto' }} className={open ? activeSide : hiddenSide}>
-        {open && (
-          <div className='relative h-full'>
-            <h1 className='pt-2 mb-2 text-lg'>
-              <a className='text-leva-light-grey' href={'https://quaternius.com/'}>
-                Free 3D Models by <span>@Quaternius</span>
-              </a>
-            </h1>
-
-            <div>
-              {Object.keys(AllModels).map((name) => {
-                const pack_name = capital(name)
-                return (
-                  <div key={pack_name} className={id === name ? 'underline decoration-solid' : ''}>
-                    <Link href={`/${name}`} as={`/${name}`}>
-                      {pack_name}
-                    </Link>
-                  </div>
-                )
-              })}
-            </div>
-            <footer className='absolute bottom-0 left-0 z-30 text-leva-light-grey'>
-              <div>
-                <a href={'https://trebeljahr.com'}>
-                  Made with <span style={{ color: ' #F35269' }}>♥</span> by Rico
-                </a>
-              </div>
-            </footer>
-          </div>
-        )}
-      </div>
-    </>
-  )
-}
-
-type Ids = keyof typeof AllModels
-
-function CanvasComponent({ id }: { id: Ids }) {
-  const [state, setState] = useState(0)
-  const [components, setComponents] = useState<{ Component: ComponentType<GroupProps>; key: string }[]>([])
-  const modelRef = useRef<Group>()
-  const stopped = useRef(false)
-
   useEffect(() => {
     const selectedPack = AllModels[id]
-    console.log(selectedPack)
     setComponents(
       Object.entries(selectedPack).map(([key, Component]) => {
-        console.log(Component)
         return { key, Component }
       }),
     )
   }, [id])
 
   const gotoPrev = useCallback(() => {
-    setState((old) => {
+    setIndex((old) => {
       const next = old - 1
       if (next < 0) return components.length - 1
       return next
@@ -115,7 +76,7 @@ function CanvasComponent({ id }: { id: Ids }) {
   }, [components.length])
 
   const gotoNext = useCallback(() => {
-    setState((old) => {
+    setIndex((old) => {
       const next = old + 1
       if (next >= components.length) return 0
       return next
@@ -123,7 +84,7 @@ function CanvasComponent({ id }: { id: Ids }) {
   }, [components.length])
 
   useEffect(() => {
-    setState(0)
+    setIndex(0)
     function handleKeyDown(event: KeyboardEvent) {
       if (event.key === 'ArrowRight') {
         gotoNext()
@@ -140,13 +101,12 @@ function CanvasComponent({ id }: { id: Ids }) {
   }, [components.length, gotoNext, gotoPrev, stopped])
 
   useEffect(() => {
-    components.forEach(({ Component }) => {
-      // @ts-ignore: next-line
-      Component.render.preload()
-    })
+    components.forEach(({ Component }) =>
+      (Component as typeof Component & { render: { preload(): void } }).render.preload(),
+    )
   }, [components])
 
-  const Model = components[state]
+  const Model = components[index]
 
   useFrame((_, delta) => {
     if (modelRef.current?.rotation && !stopped.current) {
@@ -157,16 +117,14 @@ function CanvasComponent({ id }: { id: Ids }) {
   return (
     <>
       <In>
-        <div className='absolute left-0 z-20 flex justify-center w-full bottom-1'>
-          {Model && (
-            <a href={`/glb/${id}/${Model.key}.glb`} download>
-              <span>
-                <DownloadIcon />
-                {'.glb'}
-              </span>
-            </a>
-          )}
-        </div>
+        {Model && (
+          <a className={`${downloadStyles} ${open && 'translate-x-60'}`} href={`/glb/${id}/${Model.key}.glb`} download>
+            <span>
+              <DownloadIcon />
+              {'.glb'}
+            </span>
+          </a>
+        )}
         <div className='absolute bottom-0 right-0 z-20'>
           <button className={navButton} onClick={gotoPrev}>
             {'<'}
@@ -174,6 +132,47 @@ function CanvasComponent({ id }: { id: Ids }) {
           <button className={navButton} onClick={gotoNext}>
             {'>'}
           </button>
+        </div>
+
+        <button id='close-btn' className={`${buttonStyle} ${open && 'translate-x-60'}`} onClick={toggleOpen}>
+          {open ? '<' : '>'}
+        </button>
+        <div className={`${sideStyle} ${!open && '-translate-x-60'}`}>
+          {open && (
+            <div className='relative h-full min-h-[750px]'>
+              <h1 className='pt-2 mb-2 text-lg'>
+                <a
+                  className='text-leva-light-grey hover:underline hover:decoration-solid'
+                  href={'https://quaternius.com/'}>
+                  Free 3D Models by <span>@Quaternius</span>
+                </a>
+              </h1>
+
+              <div>
+                {Object.keys(AllModels).map((name) => {
+                  const pack_name = capital(name)
+                  return (
+                    <div
+                      key={pack_name}
+                      className={`hover:text-leva-light-grey hover:underline hover:decoration-solid ${
+                        id === name && 'underline decoration-solid'
+                      }`}>
+                      <Link href={`/${name}`} as={`/${name}`}>
+                        {pack_name}
+                      </Link>
+                    </div>
+                  )
+                })}
+              </div>
+              <footer className='absolute bottom-0 left-0 z-30 text-leva-light-grey'>
+                <div>
+                  <a href={'https://trebeljahr.com'}>
+                    Built with <span style={{ color: ' #F35269' }}>♥</span> by Rico
+                  </a>
+                </div>
+              </footer>
+            </div>
+          )}
         </div>
       </In>
       <Stage intensity={0.5} preset='rembrandt' shadows={true} environment='city'>
